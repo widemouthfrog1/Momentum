07860e48e07749d8fe103d37c2e9e7e0dcc5ffb9
Karl Bennett <widemouthfrog1@gmail.com>
2019-08-12 16:22:44 +1200

    #4 Add Checkpoints

---
+++ Assets/Kill_Script.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill_Script : MonoBehaviour
+{
+    public GameObject checkpoints;
+    public GameObject pistons;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        BoxCollider2D playerBoxCollider = player.GetComponent<BoxCollider2D>();
+        CircleCollider2D playerCircleCollider = player.GetComponent<CircleCollider2D>();
+        if (playerCircleCollider.enabled)
+        {
+            if (collider.IsTouching(playerCircleCollider))
+            {
+                kill();
+            }
+        }
+        else
+        {
+            if (collider.IsTouching(playerBoxCollider))
+            {
+                kill();
+            }
+        }
+    }
+
+    private void kill()
+    {
+        Rigidbody2D playerRigidbody = player.GetComponent<Rigidbody2D>();
+        GameObject checkpoint = null;
+        foreach (Transform child in checkpoints.transform)
+        {
+            if (child.gameObject.GetComponent<Checkpoint_Script>().activated)
+            {
+                checkpoint = child.gameObject;
+            }
+        }
+        player.transform.position = new Vector2(checkpoint.transform.position.x, checkpoint.transform.position.y);
+        playerRigidbody.angularVelocity = 0;
+        playerRigidbody.velocity = new Vector2(0, 0);
+        foreach (Transform child in pistons.transform)
+        {
+            child.position = new Vector2(checkpoint.transform.position.x, checkpoint.transform.position.y);
+        }
+    }
+}
---
+++ Assets/Scripts/Checkpoint_Script.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint_Script : MonoBehaviour
+{
+    public GameObject player;
+    public bool activated;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        activated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        BoxCollider2D playerBoxCollider = player.GetComponent<BoxCollider2D>();
+        CircleCollider2D playerCircleCollider = player.GetComponent<CircleCollider2D>();
+        if (playerCircleCollider.enabled)
+        {
+            if (collider.IsTouching(playerCircleCollider) && !activated) //"!activated" is unnecessary but will improve performance for a very large number of checkpoints
+            {
+                activate();
+            }
+        }
+        else
+        {
+            if (collider.IsTouching(playerBoxCollider) && !activated)
+            {
+                activate();
+            }
+        }
+    }
+
+    private void activate()
+    {
+        foreach (Transform child in transform.parent)
+        {
+            child.gameObject.GetComponent<Checkpoint_Script>().activated = false;
+        }
+        activated = true;
+    }
+}

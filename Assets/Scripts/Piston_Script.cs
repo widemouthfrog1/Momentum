b2030dd8e451ec546d79c10e98704579a7585e4b
Karl Bennett <widemouthfrog1@gmail.com>
2019-08-12 12:45:25 +1200

    Initial commit

---
+++ Assets/Scripts/Piston_Script.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piston_Script : MonoBehaviour
+{
+    public GameObject player;
+    public string direction;//up,down,left,right
+    public float extendSpeed;
+    public float extendForce;
+    public float retractSpeed;
+    public float retractForce;
+
+    public bool extended;//true if partially or fully extended
+
+    private enum State {RETRACTED, RETRACTING, EXTENDED, EXTENDING}
+    private State state;
+
+    public bool buttonHeld;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SliderJoint2D slider = GetComponent<SliderJoint2D>();
+        //translate and rotate to correct position
+        extended = false;
+        state = State.RETRACTED;
+        buttonHeld = false;
+        //temp code:
+
+        if (direction == "Up")
+        {
+            slider.angle = 270;//rotate limit vector down so piston can move freely upward
+        }
+        if (direction == "Down")
+        {
+            slider.angle = 90;//rotate limit vector up so piston can move freely downward
+        }
+        if (direction == "Left")
+        {
+            slider.angle = 0;//rotate limit vector right so piston can move freely left
+        }
+        if (direction == "Right")
+        {
+            slider.angle = 180;//rotate limit vector left so piston can move freely right
+        }
+        Debug.Log(slider.angle);
+        //set limits to zero so pistons don't randomy slide out
+        JointTranslationLimits2D limits = new JointTranslationLimits2D();
+        limits.max = 0;
+        limits.min = 0;
+        slider.limits = limits;
+        slider.useMotor = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleControls();
+    }
+
+    void FixedUpdate()
+    {
+
+        SliderJoint2D slider = GetComponent<SliderJoint2D>();
+        CircleCollider2D playerCircleCollider = player.GetComponent<CircleCollider2D>();
+        Debug.Log(slider.angle);
+        if (!playerCircleCollider.enabled)
+        {
+            //Finite State Machine:
+            if (state == State.RETRACTED)
+            {
+                if (buttonHeld)
+                {
+                    JointTranslationLimits2D limits = new JointTranslationLimits2D();
+                    limits.max = 0.3f;
+                    limits.min = 0;
+                    slider.limits = limits;
+                    state = State.EXTENDING;
+                }
+            }
+            else if (state == State.EXTENDED)
+            {
+                if (!buttonHeld)
+                {
+                    FixedJoint2D joint = GetComponent<FixedJoint2D>();
+                    joint.enabled = false;
+                    slider.useMotor = true;
+                    state = State.RETRACTING;
+                }
+            }
+            else if (state == State.EXTENDING)
+            {
+                // Debug.Log(slider.jointTranslation);
+                if (slider.jointTranslation >= 0.3f) //assuming 0.3 is max
+                {
+                    FixedJoint2D joint = GetComponent<FixedJoint2D>();
+                    joint.enabled = true;
+                    state = State.EXTENDED;
+                    slider.useMotor = false;
+                }
+                else if (!buttonHeld)
+                {
+                    state = State.RETRACTING;
+                }
+                else
+                {
+                    extendPiston();
+                }
+            }
+            else if (state == State.RETRACTING)
+            {
+                if (slider.jointTranslation <= 0) //assuming 0 is min
+                {
+                    JointTranslationLimits2D limits = new JointTranslationLimits2D();
+                    limits.max = 0;
+                    limits.min = 0;
+                    slider.limits = limits;
+                    state = State.RETRACTED;
+                }
+                else if (buttonHeld)
+                {
+                    state = State.EXTENDING;
+                }
+                else
+                {
+                    retractPiston();
+                }
+            }
+        }
+    }
+
+    private void handleControls()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            buttonHeld = true;
+            extended = true;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            buttonHeld = false;
+        }
+    }
+
+    private void extendPiston()
+    {
+        SliderJoint2D slider = GetComponent<SliderJoint2D>();
+        JointMotor2D motor = new JointMotor2D();
+        motor.maxMotorTorque = extendForce;
+        motor.motorSpeed = extendSpeed;
+        slider.motor = motor;
+    }
+
+    private void retractPiston()
+    {
+        SliderJoint2D slider = GetComponent<SliderJoint2D>();
+        JointMotor2D motor = new JointMotor2D();
+        motor.maxMotorTorque = retractForce;
+        motor.motorSpeed = -retractSpeed;//needs to be negative as going in other direction
+        slider.motor = motor;
+    }
+}
---
+++ Assets/Scripts/Player_Script.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum PLAYER_MODE {SQUARE, CIRCLE}
+
+public class Player_Script : MonoBehaviour
+{
+    //public variables
+    public Sprite squareSprite;
+    public Color squareColour;
+    public Sprite circleSprite;
+    public Color circleColour;
+
+    public GameObject piston1;
+
+    //private variables
+    private PLAYER_MODE mode;
+
+
+    private float rotation;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialiseVariables();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //FixedUpdate is called once every physics calculation
+    void FixedUpdate()
+    {
+        handleControls();
+        updateSprite();
+        updateColliders();
+
+        Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.AddTorque(rotation);
+    }
+
+    private void handleControls()
+    {
+        if (Input.GetButtonDown("Transform"))
+        {
+            if(mode == PLAYER_MODE.SQUARE)
+            {
+                FixedJoint2D pistonJoint = piston1.GetComponent<FixedJoint2D>();
+                if (pistonJoint.connectedAnchor.y < 0.001)
+                {
+                    mode = PLAYER_MODE.CIRCLE;
+                }
+            }
+            else
+            {
+                mode = PLAYER_MODE.SQUARE;
+            }
+            
+        }
+        rotation = -Input.GetAxis("Horizontal");
+    }
+
+    private void updateSprite()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (mode == PLAYER_MODE.SQUARE)
+        {
+            if (!spriteRenderer.sprite.Equals(squareSprite))
+            {
+                spriteRenderer.sprite = squareSprite;
+                spriteRenderer.color = squareColour;
+            }
+        }
+        if (mode == PLAYER_MODE.CIRCLE)
+        {
+            if (!spriteRenderer.sprite.Equals(circleSprite))
+            {
+                spriteRenderer.sprite = circleSprite;
+                spriteRenderer.color = circleColour;
+            }
+        }
+    }
+
+    private void updateColliders()
+    {
+        BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
+        CircleCollider2D circleCollider = GetComponent<CircleCollider2D>();
+        PolygonCollider2D polygonCollider = GetComponent<PolygonCollider2D>();
+        if (mode == PLAYER_MODE.SQUARE)
+        {
+            if (boxCollider.enabled == false)
+            {
+                boxCollider.enabled = true;
+            }
+            circleCollider.enabled = false;
+        }
+        if (mode == PLAYER_MODE.CIRCLE)
+        {
+            if (circleCollider.enabled == false)
+            {
+                circleCollider.enabled = true;
+            }
+            boxCollider.enabled = false;
+        }
+    }
+
+    private void initialiseVariables()
+    {
+        //defaults to square when player is created
+        mode = PLAYER_MODE.SQUARE;
+        rotation = 0;
+    }
+}

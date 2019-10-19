a6597530c21d716e62b25589f005772d3b11c814
Guest User <Guest@wg401-b-02-06.mddn.vuw.ac.nz>
2019-10-19 16:30:44 +1300

    Wall script create child walls that fixed joints to hold the whole wall
    Former-commit-id: 3d8a61e8fd42b8f1ec7a154a78bad690cdee7a40

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -3,37 +3,212 @@ using System.Collections;
 
 public class Wall : MonoBehaviour
 {
-    public AudioClip chopSound1;                //1 of 2 audio clips that play when the wall is attacked by the player.
-    public AudioClip chopSound2;                //2 of 2 audio clips that play when the wall is attacked by the player.
-    public Sprite dmgSprite;                    //Alternate sprite to display after Wall has been attacked by player.
-    public int hp = 3;                          //hit points for the wall.
+    [SerializeField]
+    private Sprite wallSprite;
 
+    [SerializeField]
+    private float width;
 
-    private SpriteRenderer spriteRenderer;      //Store a component reference to the attached SpriteRenderer.
+    [SerializeField]
+    private float height;
+
+    [SerializeField]
+    private int rows;
+
+    [SerializeField]
+    private int cols;
+
+    [SerializeField]
+    private float breakForce;
+
+    [SerializeField]
+    private float edgeBreakForce;
+
+    private GameObject[][] walls;
 
 
     void Awake()
     {
-        //Get a component reference to the SpriteRenderer.
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+        walls = new GameObject[cols][];
+        for (int i = 0; i < cols; i++)
+        {
+            walls[i] = new GameObject[rows];
+        }
+
+        for (int x = 0; x < cols; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                GameObject o = new GameObject("Wall" + x + y);
+                o.transform.parent = transform;
+                o.AddComponent<SpriteRenderer>();
+                SpriteRenderer r = o.GetComponent<SpriteRenderer>();
+                r.sprite = wallSprite;
+                o.transform.localScale = new Vector3(width, height, 1.0f);
+                o.transform.position = transform.position;
+                o.transform.Translate(new Vector3(x * width, y * height, 0));
+
+                o.AddComponent<BoxCollider2D>();
+                o.AddComponent<Rigidbody2D>();
+                Rigidbody2D rigidbodyComponent = o.GetComponent<Rigidbody2D>();
+                rigidbodyComponent.useAutoMass = true;
+
+                if (x > 0)
+                {
+                    o.AddComponent<FixedJoint2D>();
+
+                    Component[] components = o.GetComponents(typeof(FixedJoint2D));
+                    FixedJoint2D fixedJoint = (FixedJoint2D)components[0];
+                    fixedJoint.connectedBody = walls[x - 1][y].GetComponent<Rigidbody2D>();
+                    if(y > rows / 2)
+                    {
+                        fixedJoint.breakForce = breakForce * (y - rows/2 + 1);
+                    }
+                    else
+                    {
+                        fixedJoint.breakForce = breakForce * (rows/2 - y + 1);
+                    }
+                }
+
+                if (y > 0)
+                {
+                    o.AddComponent<FixedJoint2D>();
+
+                    Component[] components = o.GetComponents(typeof(FixedJoint2D));
+                    FixedJoint2D fixedJoint;
+                    if (x > 0)
+                    {
+                        fixedJoint = (FixedJoint2D)components[1];
+                    }
+                    else
+                    {
+                        fixedJoint = (FixedJoint2D)components[0];
+                    }
+                    fixedJoint.connectedBody = walls[x][y - 1].GetComponent<Rigidbody2D>();
+                    if (y > rows / 2)
+                    {
+                        fixedJoint.breakForce = breakForce * (y - rows / 2 + 1);
+                    }
+                    else
+                    {
+                        fixedJoint.breakForce = breakForce * (rows / 2 - y + 1);
+                    }
+
+                }
+
+                walls[x][y] = o;
+
+            }
+        }
+        if (cols > rows)
+        {
+            GameObject o = new GameObject("LeftWall");
+            o.transform.parent = transform;
+            o.AddComponent<SpriteRenderer>();
+            SpriteRenderer r = o.GetComponent<SpriteRenderer>();
+            r.sprite = wallSprite;
+
+            o.transform.localScale = new Vector3(width, height * rows, 1.0f);
+            o.transform.position = transform.position;
+            o.transform.Translate(new Vector3(-1 * width, 0.5f * rows * height, 0));
+
+            o.AddComponent<BoxCollider2D>();
+            r.enabled = false;
+            o.AddComponent<Rigidbody2D>();
+            Rigidbody2D rigidbodyComponent = o.GetComponent<Rigidbody2D>();
+            rigidbodyComponent.useAutoMass = true;
+            rigidbodyComponent.bodyType = RigidbodyType2D.Static;
+            for (int i = 0; i < rows; i++)
+            {
+                o.AddComponent<FixedJoint2D>();
+
+                Component[] components = o.GetComponents(typeof(FixedJoint2D));
+                FixedJoint2D fixedJoint = (FixedJoint2D)components[i];
+                fixedJoint.connectedBody = walls[0][i].GetComponent<Rigidbody2D>();
+                fixedJoint.breakForce = edgeBreakForce;
+            }
+
+            o = new GameObject("RightWall");
+            o.transform.parent = transform;
+            o.AddComponent<SpriteRenderer>();
+            r = o.GetComponent<SpriteRenderer>();
+            r.sprite = wallSprite;
+            o.transform.localScale = new Vector3(width, height * rows, 1.0f);
+            o.transform.position = transform.position;
+            o.transform.Translate(new Vector3(cols * width, 0.5f * rows * height, 0));
+
+            o.AddComponent<BoxCollider2D>();
+            r.enabled = false;
+            o.AddComponent<Rigidbody2D>();
+            rigidbodyComponent = o.GetComponent<Rigidbody2D>();
+            rigidbodyComponent.useAutoMass = true;
+            rigidbodyComponent.bodyType = RigidbodyType2D.Static;
+            for (int i = 0; i < rows; i++)
+            {
+                o.AddComponent<FixedJoint2D>();
+
+                Component[] components = o.GetComponents(typeof(FixedJoint2D));
+                FixedJoint2D fixedJoint = (FixedJoint2D)components[i];
+                fixedJoint.connectedBody = walls[cols-1][i].GetComponent<Rigidbody2D>();
+                fixedJoint.breakForce = edgeBreakForce;
+            }
 
 
-    //DamageWall is called when the player attacks a wall.
-    public void DamageWall(int loss)
-    {
-        //Call the RandomizeSfx function of SoundManager to play one of two chop sounds.
-        //SoundManager.instance.RandomizeSfx(chopSound1, chopSound2);
+        }
+        else
+        {
+            GameObject o = new GameObject("BottomWall");
+            o.transform.parent = transform;
+            o.AddComponent<SpriteRenderer>();
+            SpriteRenderer r = o.GetComponent<SpriteRenderer>();
+            r.sprite = wallSprite;
 
-        //Set spriteRenderer to the damaged wall sprite.
-        spriteRenderer.sprite = dmgSprite;
+            o.transform.localScale = new Vector3(width * cols, height, 1.0f);
+            o.transform.position = transform.position;
+            o.transform.Translate(new Vector3(0.5f * width * cols, -1f * height, 0));
 
-        //Subtract loss from hit point total.
-        hp -= loss;
+            o.AddComponent<BoxCollider2D>();
+            r.enabled = false;
+            o.AddComponent<Rigidbody2D>();
+            Rigidbody2D rigidbodyComponent = o.GetComponent<Rigidbody2D>();
+            rigidbodyComponent.useAutoMass = true;
+            rigidbodyComponent.bodyType = RigidbodyType2D.Static;
+            for (int i = 0; i < cols; i++)
+            {
+                o.AddComponent<FixedJoint2D>();
 
-        //If hit points are less than or equal to zero:
-        if (hp <= 0)
-            //Disable the gameObject.
-            gameObject.SetActive(false);
+                Component[] components = o.GetComponents(typeof(FixedJoint2D));
+                FixedJoint2D fixedJoint = (FixedJoint2D)components[i];
+                fixedJoint.connectedBody = walls[i][0].GetComponent<Rigidbody2D>();
+                fixedJoint.breakForce = edgeBreakForce;
+            }
+
+            o = new GameObject("TopWall");
+            o.transform.parent = transform;
+            o.AddComponent<SpriteRenderer>();
+            r = o.GetComponent<SpriteRenderer>();
+            r.sprite = wallSprite;
+            o.transform.localScale = new Vector3(width * cols, height, 1.0f);
+            o.transform.position = transform.position;
+            o.transform.Translate(new Vector3(0.5f * cols * width, rows * height, 0));
+
+            o.AddComponent<BoxCollider2D>();
+            r.enabled = false;
+            o.AddComponent<Rigidbody2D>();
+            rigidbodyComponent = o.GetComponent<Rigidbody2D>();
+            rigidbodyComponent.useAutoMass = true;
+            rigidbodyComponent.bodyType = RigidbodyType2D.Static;
+            for (int i = 0; i < cols; i++)
+            {
+                o.AddComponent<FixedJoint2D>();
+
+                Component[] components = o.GetComponents(typeof(FixedJoint2D));
+                FixedJoint2D fixedJoint = (FixedJoint2D)components[i];
+                fixedJoint.connectedBody = walls[i][rows-1].GetComponent<Rigidbody2D>();
+                fixedJoint.breakForce = edgeBreakForce;
+            }
+        }
+
+
     }
 }

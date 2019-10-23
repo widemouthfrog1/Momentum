436e4250f69d7fea6804eb76d5aadb1d487c801b
Karl Bennett <widemouthfrog1@gmail.com>
2019-10-23 15:21:50 +1300

    Remake Level 1 and fix level template
    Former-commit-id: 4c5e6981d1cd673e76aced1f6fc46a6590ab10ce

--- Assets/Scripts/Player_Script.cs
+++ Assets/Scripts/Player_Script.cs
@@ -59,12 +59,6 @@ public class Player_Script : MonoBehaviour
             
             wasCircleLastTick = false;
         }
-        int i = 1;
-        foreach (Transform child in pistons.transform)
-        {
-            Debug.Log("Piston" + i + ": " + child.GetComponent<Rigidbody2D>().rotation);
-            i++;
-        }
         //Debug.Log(rigidBody.rotation);
 
         rigidBody.AddTorque(angularAcceleration);
